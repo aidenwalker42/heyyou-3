6815e7eb008c19b4ce83e0753610bf5a7600e7bf
Ant-Pearl <carlosalvatici@gmail.com>
2024-12-18 15:13:52 -0800

    server small changes

--- routes/api/customerContact.js
+++ routes/api/customerContact.js
@@ -11,7 +11,26 @@ router.get('/', async (req, res) => {
 
 //Add Post
 router.post('/', async (req, res) => {
+    const customerContactCol = await loadCustomerContactCollection()
 
+    // Checking for string only
+    if (
+        typeof req.body.name == "string" &&
+        typeof req.body.email == "string" &&
+        typeof req.body.message == "string"
+    ) {
+        // Inserting new document
+        customerContactCol.insertOne({ 
+            name: req.body.name,
+            email: req.body.email,
+            message: req.body.message
+        })
+        // Success
+        res.status(201).send({msg: "Update Successful!"})
+    } else {
+        // Invalid type
+        res.status(406).send({msg: "Invalid Data"})
+    }
 })
 
 //Update Post
