d800578475593ddc60d53de53660ab3427490637
Ant-Pearl <carlosalvatici@gmail.com>
2024-12-18 16:37:53 -0800

    changed index file serve front-end/index.html

--- index.js
+++ index.js
@@ -27,10 +27,10 @@ app.use('/api/websiteModification', websiteModification)
 
 if(process.env.NODE_ENV === 'production') {
   // Static Folder
-  app.use(express.static(path.join(__dirname + '/public/')))
+  app.use(express.static(path.join(__dirname + '/front-end/')))
 
   // Handle SPA
-  app.get(/.*/, (req, res) => res.sendFile(path.join(__dirname + '/public/index.html')))
+  app.get(/.*/, (req, res) => res.sendFile(path.join(__dirname + '/front-end/index.html')))
 }
 
 const port = process.env.PORT || 5000
