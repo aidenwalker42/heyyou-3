b892231c51db6e9c1e6a15a6d5ee9c9b05129273
Ant-Pearl <carlosalvatici@gmail.com>
2024-12-25 20:58:25 -0800

    switched to heroku api address for heroku push

--- front-end/assets/js/api.js
+++ front-end/assets/js/api.js
@@ -1,9 +1,9 @@
-let herokuAPI = "heroku address"
+let herokuAPI = "https://hey-you-3-4f07e539bb50.herokuapp.com/api/"
 let localAPI = "http://localhost:5000/api/"
 
 const getAPI = () => {
-    //return herokuAPI
+    return herokuAPI
     return localAPI
 }
-
+// NIgga
 export {getAPI}
