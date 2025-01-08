c0f8dc1b61838762624b69f387147711027a94e0
Ant-Pearl <carlosalvatici@gmail.com>
2025-01-07 20:54:52 -0800

    Built backend for page editor

--- routes/api/websiteModification.js
+++ routes/api/websiteModification.js
@@ -1,36 +1,99 @@
 import express from 'express'
 
-import {loadCustomerContactCollection, loadWebsiteModificationCollection} from '../../server/loadCollections.js';
+import { checkCredentials } from '../../server/checkCredentials.js';
+import {loadWebsiteModificationCollection} from '../../server/loadCollections.js';
 
 
 const router = express.Router()
 //Get Posts
 router.get('/', async (req, res) => {
-    res.status(201).send("Hello world")
+    let data
+    switch(req.body.getType) {
+        case "getNames":
+            data = await getNames()
+        case "getProductData":
+            data = await getProductData(req.body.id)
+        case "getAllProducts":
+            data = await getAllProducts()
+        default:
+            res.status(401).send({msg: "Invalid getType"})
+    }
+    res.status(201).send(data)
 })
 
-//Add Post
+// Add Post
+// New Product
 router.post('/', async (req, res) => {
-    //Need to categorize....
-    //Based on page??
-    let exampleData = {
-        pages: {
-            "home": {
-                Title: ""
-            },
-            "brands": [
-                {
-                    name: "",
-                    mainDesc1: "",
-                }
-            ]
+    // Give Pages Id's
+    if (await checkCredentials(req.body.username, req.body.password)) {
+        const websiteModPosts = await loadWebsiteModificationCollection()
+
+        // Checking for duplicate id
+        let websiteData = await websiteModPosts.find({id: req.body.id}).toArray()
+        if(websiteData.length == 0){
+            await websiteModPosts.insertOne(req.body.post)
+        } else {
+            res.status(406).send({msg: "Duplicate id"})
         }
+        
     }
 })
 
-//Update Post
+// Update Post
+// Update Product
+router.put('/', async (req, res) => {
+    // Give Pages Id's
+    if (await checkCredentials(req.body.username, req.body.password)) {
+        const websiteModPosts = await loadWebsiteModificationCollection()
+
+        let websiteData = await websiteModPosts.updateOne({id: req.body.id}, req.body.post)
+        res.status(201).send("Post Updated")
+    }
+})
 
 //Delete Post
+router.delete('/', async (req, res) => {
+    // Give Pages Id's
+    if (await checkCredentials(req.body.username, req.body.password)) {
+        const websiteModPosts = await loadWebsiteModificationCollection()
+
+        let websiteData = await websiteModPosts.remove({id: req.body.id})
+        res.status(201).send("Post Removed")
+    }
+})
+
+// Get Requests
+// Get id's for product list.
+async function getIds() {
+    const websiteModPosts = await loadWebsiteModificationCollection()
+    
+    let posts = websiteModPosts.find({}).toArray()
+
+    // Building Name list from list of posts.
+    let idsList = []
+    for (let post of posts) {
+        idsList.push(post.id)
+    }
+    return idsList
+}
+
+async function getAllProducts() {
+    const websiteModPosts = await loadWebsiteModificationCollection()
+
+    let posts = await websiteModPosts.find({}).toArray()
+
+    return posts
+}
+
+// Get product data for whatever bullshit
+async function getProductData(id) {
+    const websiteModPosts = await loadWebsiteModificationCollection()
+    
+    // Simple, getting and returning data of the associated id.
+    let post = await websiteModPosts.find({id: id}).toArray()
+
+    return post[0]
+}
 
 
 export {router}
