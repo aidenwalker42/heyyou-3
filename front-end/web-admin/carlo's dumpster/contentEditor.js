de3ca5d590b50876739ca3cb593b070b92219ca6
Aiden Walker <haxor.pixel@gmail.com>
2025-01-09 17:36:04 -0600

    carlos dumpster contenteditor.js

---
+++ front-end/web-admin/carlo's dumpster/contentEditor.js
@@ -0,0 +1,13 @@
+// 	document.getElementById("page-selector").value
+//   loadBrandData()
+// })
+
+// let listOfElements = ["header-input", "subtext-textarea", "banner-image", "our-story-header", "our-story-subtext",
+//   "footer-header", "footer-subtext", "footer-btn-link", "footer-enable-btn"
+// ]
+// document.getElementById("create-page-btn").addEventListener("click", () => {
+//   // console.log("Create Page")
+//   for (let element of listOfElements) {
+//     document.getElementById(element).value = ""
+//   }
+// })
